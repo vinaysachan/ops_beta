95157644c80f68d263312dfeacc914ff3cb498ad
Vinay Sachan <vinaysachan@live.in>
2015-09-15 12:15:01 +0530

    admin_09_15

--- html/ops_admin/assets/js/admin.js
+++ html/ops_admin/assets/js/admin.js
@@ -1,17 +1,31 @@
 $('.left_menu li.active').addClass('open');
 $('.left_menu > li > a').bind('click', function () {
     if ($(this).parent().children('ul').hasClass('sub-menu') && (!$('body').hasClass('navigation-small') || !$(this).parent().parent().hasClass('left_menu'))) {
-        if (!$(this).parent().hasClass('open')) {
-            $(this).parent().addClass('open');
-            $(this).parent().parent().children('li.open').not($(this).parent()).not($('.left_menu > li.active')).removeClass('open').children('ul').slideUp(500);
-            $(this).parent().children('ul').slideDown(500, function () { });
-        } else {
-            if (!$(this).parent().hasClass('active')) {
-                $(this).parent().parent().children('li.open').not($('.left_menu > li.active')).removeClass('open').children('ul').slideUp(500, function () { });
-            } else {
-                $(this).parent().parent().children('li.open').removeClass('open').children('ul').slideUp(500, function () { });
-            }
-        }
+	if (!$(this).parent().hasClass('open')) {
+	    $(this).parent().addClass('open');
+	    $(this).parent().parent().children('li.open').not($(this).parent()).not($('.left_menu > li.active')).removeClass('open').children('ul').slideUp(500);
+	    $(this).parent().children('ul').slideDown(500, function () {
+	    });
+	} else {
+	    if (!$(this).parent().hasClass('active')) {
+		$(this).parent().parent().children('li.open').not($('.left_menu > li.active')).removeClass('open').children('ul').slideUp(500, function () {
+		});
+	    } else {
+		$(this).parent().parent().children('li.open').removeClass('open').children('ul').slideUp(500, function () {
+		});
+	    }
+	}
     }
 });
-    
+
+$('.left_toggler').bind('click', function () {
+    if ($('.left_toggler').hasClass('hide_left')) {
+	$('.left_toggler').html('<i class="fa fa-times"></i>').removeClass('hide_left');
+	$('.aside').css('display', 'block');
+	$('.pageContent').addClass('extended'); 
+    } else {
+	$('.left_toggler').html('<i class="fa fa-outdent"></i>').addClass('hide_left');
+	$('.aside').css('display', 'none');
+	$('.pageContent').removeClass('extended'); 
+    }
+});
