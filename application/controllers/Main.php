96389124dc5607a30008e5eb31f50f78530842e0
Vinay Sachan <vinaysachan@live.in>
2015-09-18 12:28:42 +0530

    Model Added

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -5,7 +5,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Main extends CI_Controller {
 
     public function index() {
-        $this->load->view('main/index_view');
+	$this->load->model('test_model');
+	$r= $this->test_model->testing();
+	
+	print_r($r);
+	$this->load->view('welcome_message');
     }
 
 }
---
+++ application/models/Test_model.php
@@ -0,0 +1,18 @@
+<?php
+
+class Test_model extends CI_Model {
+
+    public $id;
+    public $name; 
+
+    public function __construct() {
+	// Call the CI_Model constructor
+	parent::__construct();
+    }
+
+    public function testing() {
+	$query = $this->db->get('tbl_test', 10);
+	return $query->result();
+    }
+
+}
